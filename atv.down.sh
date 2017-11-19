4b4c0e5c75766af04a2fd3a960e7d5c94250db90
felho <felho@ustream.tv>
2017-11-19 17:44:49 +0100

    if the download of a show fails, we don't add it to the log, alos we retry once

--- atv.down.sh
+++ atv.down.sh
@@ -19,6 +19,21 @@ while read TABLE; do
 	OUTPUT="$BASE_DIR/$DAY/$OUTPUT_FILE"
 
 	rtmpdump -W "http://www.atv.hu/js/flowplayer/flowplayer-3.2.15.swf" --tcUrl="rtmp://$SERVERIP/mediacache/_definst_" --pageUrl="http://www.atv.hu/videok" -r "rtmp://$SERVERIP/mediacache/_definst_" -y "mp4:atv/$INPUT.mp4" --live -o "$OUTPUT.mp4"
+	EXIT_CODE=$?
 
-	echo "$TABLE - $?" >> $FINISHED_LIST
+	if [ $EXIT_CODE -ne 0 ]
+  then
+    echo "$* failed with exit code $EXIT_CODE - retry"
+		#The reason why we retry once is that in majority of the cases the issue behind a failed download is closing the lid of the laptop, switching wifi etc.
+		rtmpdump -W "http://www.atv.hu/js/flowplayer/flowplayer-3.2.15.swf" --tcUrl="rtmp://$SERVERIP/mediacache/_definst_" --pageUrl="http://www.atv.hu/videok" -r "rtmp://$SERVERIP/mediacache/_definst_" -y "mp4:atv/$INPUT.mp4" --live -o "$OUTPUT.mp4"
+		EXIT_CODE=$?
+		if [ $EXIT_CODE -ne 0 ]
+	  then
+	    echo "$* failed with exit code $EXIT_CODE - go for next item"
+	  else
+			echo "$TABLE - $EXIT_CODE" >> $FINISHED_LIST
+	  fi
+  else
+		echo "$TABLE - $EXIT_CODE" >> $FINISHED_LIST
+  fi
 done < $INPUT_LIST
